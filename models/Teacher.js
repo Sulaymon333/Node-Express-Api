9c3aebb0a04aa5ff46ea3ff7642f2e9f6972b200
Sulaymon333 <tjsulaymon@gmail.com>
2019-06-04 12:26:28 +0300

    express app ready to deploy to heroku

---
+++ models/Teacher.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const teacherSchema = new Schema({
+    name: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    bio: {
+        type: String
+    },
+    courses: {
+        type: Array
+    },
+    createAt: {
+        type: Date
+    }
+});
+
+module.exports = Teacher = mongoose.model('teacher', teacherSchema); // to create teacher schema/ database to mongodb
